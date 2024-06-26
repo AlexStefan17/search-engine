9f9f523150369689024240a66da312e9e0018ed7
Alex Stefan <stefan.alex170299@gmail.com>
2024-06-26 20:12:47 +0300

    localhost

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.peviitor.ro/v3/search/";
+const API_URL = "http://localhost:8983/solr/#/";
 
 // Fetch the jobs using the string created by user inputs/checkbox.
 export const getData = async (createQueryString) => {
