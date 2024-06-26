57e9175ef1a6ca50bdc04d490c5cd95e17f908de
Alex Stefan <stefan.alex170299@gmail.com>
2024-06-26 20:24:32 +0300

    removed #

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8983/solr/#/";
+const API_URL = "http://localhost:8983/solr/";
 
 // Fetch the jobs using the string created by user inputs/checkbox.
 export const getData = async (createQueryString) => {
