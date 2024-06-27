d694fd96c7a79a64f5b24319b59fa1820c696a58
Alex Stefan <stefan.alex170299@gmail.com>
2024-06-27 12:13:49 +0300

    url

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,4 +1,4 @@
-const API_URL = "https://localhost:8983/solr/jobs/select?q=search_term&wt=json";
+const API_URL = "https://localhost:8983/solr/jobs/";
 
 // Fetch the jobs using the string created by user inputs/checkbox.
 export const getData = async (createQueryString) => {
